22c6841fd2c742aa573e433423c12c12a23f3316
Raghav Pahwa <raghavpahwa@outlook.com>
2020-12-29 19:01:31 +0530

    Installed and set up styled components along with basic navigation links using React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Game from "./pages/Game";
+import GameOver from "./pages/GameOver";
+import HighScores from "./pages/HighScores";
+import Home from "./pages/Home";
+
+import Navbar from "./components/Navbar";
+
+import Global from "./styled/Global";
+import { Container } from "./styled/container";
+import { Main } from "./styled/Main";
 
 function App() {
   return (
-    <div>
-    <h1> App</h1>
-    </div>
+    <Router>
+      <Main>
+        <Global />
+        <Container>
+          <Navbar />
+          <Switch>
+            <Route path="/game" component={Game} />
+            <Route path="/highscores" component={HighScores} />
+            <Route path="/gameover" component={GameOver} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </Container>
+      </Main>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+export default function Navbar() {
+    return (
+        <nav>
+            <div>
+                 <Link to="/">
+                     Learn.Build.<span>Type.</span>
+                     </Link>
+            </div>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/highscores">High Scores</Link></li>
+            </ul>
+        </nav>
+    )
+}
