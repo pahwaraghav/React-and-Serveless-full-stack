bd37f3ea22e0f932d4e890d2919bca624caf17bf
Raghav Pahwa <raghavpahwa@outlook.com>
2020-12-29 21:48:52 +0530

    Implemented Game Timer logic

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import {
   StyledGame,
@@ -9,15 +9,58 @@ import {
 
 import { Strong } from "../styled/Common";
 
-export default function Game() {
+export default function Game({ history }) {
+  const [score, setScore] = useState(0);
+  const MAX_SECONDS = 5;
+  const [ms, setMs] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const currentTime = new Date();
+    const interval = setInterval(() => updateTime(currentTime), 1);
+    return () => clearInterval(interval);
+  }, []);
+
+  const updateTime = (startTime) => {
+    const endTime = new Date();
+    const msPassedStr = (endTime.getTime() - startTime.getTime()).toString();
+    const formattedMsString = ("0000" + msPassedStr).slice(-5);
+
+    const updatedSeconds =
+      MAX_SECONDS - parseInt(formattedMsString.substring(0, 2)) - 1;
+    const updatedMs =
+      1000 -
+      parseInt(formattedMsString.substring(formattedMsString.length - 3));
+
+    setSeconds(addLeadingZeros(updatedSeconds, 2));
+    setMs(addLeadingZeros(updatedMs, 3));
+  };
+
+  const addLeadingZeros = (num, length) => {
+    let zeros = "";
+    for (let i = 0; i < length; i++) {
+      zeros += "0";
+    }
+    return (zeros + num).slice(-length);
+  };
+
+  useEffect(() => {
+    if (seconds <= -1) {
+      history.push("/gameOver");
+    }
+  }, [seconds, ms]);
+
   return (
     <StyledGame>
       <StyledScore>
-        Score:<Strong>0</Strong>
+        Score:<Strong>{score}</Strong>
       </StyledScore>
       <StyledCharacter>A</StyledCharacter>
       <StyledTimer>
-        Time: <Strong>00: 000</Strong>
+        Time:{" "}
+        <Strong>
+          {seconds}: {ms}
+        </Strong>
       </StyledTimer>
     </StyledGame>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,12 +5,10 @@ import { Accent, StyledTitle } from "../styled/Common";
 export default function Home() {
   return (
     <div>
-      <h1>
-        <StyledTitle>Ready to type?</StyledTitle>
-        <CTA to="/game">
-          Click or type <Accent>'s'</Accent> to start playing!
-        </CTA>
-      </h1>
+      <StyledTitle>Ready to type?</StyledTitle>
+      <CTA to="/game">
+        Click or type <Accent>'s'</Accent> to start playing!
+      </CTA>
     </div>
   );
 }
