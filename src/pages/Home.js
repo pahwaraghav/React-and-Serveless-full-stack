244f3d458d867c689ae3a09307f7360d1105738a
Raghav Pahwa <raghavpahwa@outlook.com>
2020-12-29 20:29:36 +0530

    Styled Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import CTA from "../styled/CTA";
+import { Accent, StyledTitle } from "../styled/Common";
 
-export default function Home(){
-    return(
-        <div>
-            <h1>
-                Home
-            </h1>
-        </div>
-    )
+export default function Home() {
+  return (
+    <div>
+      <h1>
+        <StyledTitle>Ready to type?</StyledTitle>
+        <CTA to="/game">
+          Click or type <Accent>'s'</Accent> to start playing!
+        </CTA>
+      </h1>
+    </div>
+  );
 }
