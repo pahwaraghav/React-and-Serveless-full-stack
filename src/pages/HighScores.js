973eaa99d76dc41e3777260ad3d3aff52872e42d
Raghav Pahwa <raghavpahwa@outlook.com>
2020-12-31 19:41:53 +0530

    Displayed High Scores and added saveHighScores serverless function

--- src/pages/HighScores.js
+++ src/pages/HighScores.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+
+import { ScoresList, ScoreLi } from "../styled/HighScores";
 
 export default function HighScores() {
-    return (
-        <div>
-            <h1>HIgh Scores</h1>
-        </div>
-    )
+  const [scores, setHighScores] = useState([]);
+  useEffect(() => {
+    const loadHighScores = async () => {
+      try {
+        const res = await fetch("/.netlify/functions/getHighScores");
+        const scores = await res.json();
+        setHighScores(scores);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    loadHighScores();
+  }, []);
+  return (
+    <div>
+      <h1>HIgh Scores</h1>
+      <ScoresList>
+        {scores.map((score) => (
+          <ScoreLi key={score.id}>
+            {score.fields.name} - {score.fields.score}
+          </ScoreLi>
+        ))}
+      </ScoresList>
+    </div>
+  );
 }
---
+++ src/styled/HighScores.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ScoresList = styled.ol`
+  /* padding-left: 0; */
+  /* list-style-type: none; */
+`;
+
+export const ScoreLi = styled.li`
+  font-size: 1.5rem;
+  margin-bottom: 0.5rem;
+`;
